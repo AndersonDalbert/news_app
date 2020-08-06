031d3283e3b649bb00d99c7b35657d948c2cf3ad
AndersonDalbert <dev.andersondalbert@gmail.com>
2020-08-05 21:39:25 -0300

    add edit buttoM

--- lib/screens/profile/components/header.dart
+++ lib/screens/profile/components/header.dart
@@ -12,38 +12,48 @@ class ProfileHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 225,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Theme.of(context).primaryColor,
-            Theme.of(context).accentColor,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.elliptical(60.0, 30.0),
-          bottomRight: Radius.elliptical(60.0, 30.0),
-        ),
-      ),
-      child: Column(
-        children: <Widget>[
-          SizedBox(height: 15),
-          Center(
-            child: _buildCircleAvatar(profileImageUrl),
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: 225,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Theme.of(context).primaryColor,
+                Theme.of(context).accentColor,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.elliptical(60.0, 30.0),
+              bottomRight: Radius.elliptical(60.0, 30.0),
+            ),
           ),
-          SizedBox(height: 15),
-          Text(
-            userName,
-            style: Theme.of(context)
-                .textTheme
-                .headline2
-                .copyWith(color: Colors.white),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20),
+              Center(
+                child: _buildCircleAvatar(profileImageUrl),
+              ),
+              SizedBox(height: 15),
+              Text(
+                userName,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline2
+                    .copyWith(color: Colors.white),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        Positioned(
+          top: 10,
+          right: 10,
+          child: IconButton(
+              color: Colors.white, icon: Icon(Icons.edit), onPressed: () {}),
+        ),
+      ],
     );
   }
 
